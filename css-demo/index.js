0386fd019e7c85351aa92b8aa9d4c3a8cd1e55d5
Theia Henderson <theia@mit.edu>
2026-02-25 19:26:40 -0500

    Update index.js

---
+++ css-demo/index.js
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+
+const app = createApp({
+  template: "#template",
+
+  data() {
+    return {
+      // Data here
+    };
+  },
+
+  methods: {
+    // Methods here
+  },
+}).mount("#app");
